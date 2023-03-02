3727b47da29ce92ae405598ad7515a0a95b57a85
liang-jin-hua <1849185957@qq.com>
2023-03-02 11:46:00 +0800

    梁进华账号第五次提交到分支，新建main03

---
+++ Test/app/src/main/java/com/example/gittest/MainActivity3.java
@@ -0,0 +1,17 @@
+package com.example.gittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Log.i("gitTest","main03");
+    }
+}
