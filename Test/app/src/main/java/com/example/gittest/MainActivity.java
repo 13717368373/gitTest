8a1b64b4bacda8469ae24c7661b0b92d8e85bc88
liang-jin-hua <1849185957@qq.com>
2023-03-07 17:59:52 +0800

    文光修改远程代，dialog测试onResume函数问题

--- Test/app/src/main/java/com/example/gittest/MainActivity.java
+++ Test/app/src/main/java/com/example/gittest/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.gittest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
         Log.i("gitTest","liangjinhua3333");
         Log.i("gitTest","测试冲突用_wenguang");
 
+        findViewById(R.id.btn_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+
+    }
+
+    private void showDialog() {
+        Dialog dialog = new Dialog(this, R.style.dialog_warn);
+        dialog.setContentView(R.layout.layout_delete_dialog);
+
+        dialog.findViewById(R.id.delete_data_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.setCancelable(false);
+        dialog.show();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("gitTest","onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("gitTest","onPause");
     }
 }
